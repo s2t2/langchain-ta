ec4357123930365b6fc229f53f89aeeb2b975ae7
MJ Rossetti <s2t2@users.noreply.github.com>
2023-12-14 22:15:09 -0500

    Test formatters

--- test/response_formatters_test.py
+++ test/response_formatters_test.py
@@ -1,10 +1,10 @@
 
+from langchain.output_parsers import PydanticOutputParser
 
 from app.response_formatters import Student, DocumentScoring, QuestionScoring, QuestionScorings
 
 
-def test_student_formatter():
-
+def test_models():
 
     student = Student(name="Sally Student", net_id="G123456")
     print(student)
@@ -16,4 +16,9 @@ def test_student_formatter():
     scorings = QuestionScorings(scorings=[qs])
     print(scorings)
 
-    breakpoint()
+
+def test_formatting_instructions():
+
+    parser = PydanticOutputParser(pydantic_object=Student)
+    instructions = parser.get_format_instructions()
+    assert instructions == 'The output should be formatted as a JSON instance that conforms to the JSON schema below.\n\nAs an example, for the schema {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}\nthe object {"foo": ["bar", "baz"]} is a well-formatted instance of the schema. The object {"properties": {"foo": ["bar", "baz"]}} is not well-formatted.\n\nHere is the output schema:\n```\n{"description": "A student.", "properties": {"name": {"description": "The student\'s full name.", "title": "Name", "type": "string"}, "net_id": {"description": "The student\'s identifier.", "title": "Net Id", "type": "string"}}, "required": ["name", "net_id"]}\n```'
